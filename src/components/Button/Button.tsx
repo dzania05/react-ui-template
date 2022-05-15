aeb0ea7ef833bffd8c50674bee0f6ddf00db53a3
dzania05 <pandu.whello@gmail.com>
2022-05-15 14:51:37 +0700

    add classnames library

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import classnames from 'classnames';
+import { ButtonProps } from './types/types';
 
-export interface ButtonProps {
-  label: string;
-}
-
-const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+const Button = ({
+  label,
+  color = 'primary',
+  isLight = false,
+  isRounded = false,
+  size = 'normal',
+  isOutlined = false
+}: ButtonProps) => {
+  return (
+    <button
+      className={classnames(
+        'button',
+        color && 'is-' + color,
+        size && 'is-' + size,
+        isRounded ? 'is-rounded' : '',
+        isLight ? 'is-light' : '',
+        isOutlined ? 'is-outlined' : '' 
+      )}
+    >
+      {label}
+    </button>
+  );
 };
 
 export default Button;
---
+++ src/components/Button/types/types.ts
@@ -0,0 +1,28 @@
+export enum ColorEnum {
+  Primary = 'primary',
+  Link = 'link',
+  Info = 'info',
+  Success = 'success',
+  Warning = 'warning',
+  Danger = 'danger'
+}
+
+export type Colors = `${ColorEnum}`
+
+export enum SizeEnum {
+  Small = 'small',
+  Normal = 'normal',
+  medium = 'medium',
+  Large = 'large'
+}
+
+export type Size = `${SizeEnum}`
+
+export type ButtonProps {
+  label: string,
+  color?: Colors,
+  isLight?: boolean,
+  isRounded?: boolean,
+  size?: Size,
+  isOutlined?: boolean 
+}
